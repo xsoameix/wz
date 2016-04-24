73074c41404bccc3fce23961edeb7725c829c428
Lien Chiang <xsoameix@gmail.com>
2016-04-24 18:25:16 +0800

    Suppress gcc variadic macros extension in Mac

--- tests/check.h
+++ tests/check.h
@@ -4,6 +4,16 @@
 #ifdef _WIN32
 typedef int pid_t;
 #endif
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#endif
+
 #include <check.h>
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 #endif
